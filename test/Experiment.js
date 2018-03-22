60261e10791a1b2332ae0c673b543730ebe41e78
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-03-22 13:01:15 +0800

    collect all the tests

--- test/Experiment.js
+++ test/Experiment.js
@@ -19,60 +19,76 @@ function writeToCsv(csv_file_name, data_arr) {
     file.end();
 }
 
-var NUM_TEST = 10;
 
+// ##################### EXPERIMENT SETUPS ##################### //
 var EXPERIMENT_NAME = "Exp #7 SetSeedBatch (10) Latency";
 var TESTNET = "Kovan";
+var NUM_TEST = 10;
 
+
+// ##################### CONTRACT ADDRESS ##################### //
 // var CUSTODIAN_ADDRESS = "0x48485856d3778bc1ed9837f4c09ccd59a90ed57a"; // Ropsten
 // var CUSTODIAN_ADDRESS = "0x8a916a01cf632b5980a8b525f85d7a6da689658a"; // Rinkeby
 var CUSTODIAN_ADDRESS = "0xb458781ac460d23a0ea3820b1ea1a684747ddd01"; // Kovan
 
 
+// ##################### CONTRACT TEST ##################### //
 contract('Custodian', function(accounts) {
-    
     it(EXPERIMENT_NAME, async function() {
-
         console.log(EXPERIMENT_NAME + " on <" + TESTNET + ">");
 
+
+        // ##################### CUSTODIAN (DEPLOYED / NEW) ##################### //
         var custodianInstance = await Custodian.at(CUSTODIAN_ADDRESS);
         // var custodianInstance = await Custodian.deployed();
         // var custodianInstance = await Custodian.new();
 
-        // Speedy Init 
+
+        // ##################### (Optional) CREATE CLIENTS BATCH ##################### //
         for (var i = 0; i < 20; i++) {
             await custodianInstance.createClientBatch(10);
             console.log(i);
         }
 
+        // Check Volume
         var volume = await custodianInstance.volume.call();
         console.log(volume);       
 
+        // Inits
         var start_ts;
         var exp_list = [];
 
 
-        // Swift test for long night
+
+        // ##################### TEST INITIALIZATION BEGIN ##################### //
         temp_experiment_name = ["Exp #5 SetSeedBatch (1) Latency","Exp #6 SetSeedBatch (5) Latency","Exp #7 SetSeedBatch (10) Latency","Exp #8 SetSeedBatch (20) Latency","Exp #9 SetSeedBatch (50) Latency","Exp #10 SetSeedBatch (100) Latency"]
         temp_batch_size = [1,5,10,20,50,100]
         temp_iteration = 6;
+        // ##################### TEST INITIALIZATION END ##################### //
+
 
         for (var j=0;j<temp_iteration;j++) {
-
             exp_list = [];
 
             for (var i = 0; i < NUM_TEST; i++) {
                 start_ts = getNow();
-                // await custodianInstance.setSeedBatch(i, 10); // OOO
+                
+
+                // ##################### MAIN EXECUTION TESTING BEGIN ##################### //
                 await custodianInstance.setSeedBatch(i, temp_batch_size[j]); // XXX
+                // await custodianInstance.setSeedBatch(i, 10); // OOO
+                // ##################### MAIN EXECUTION TESTING END ##################### //
+                
 
                 time_diff = getTimeDiff(start_ts);      // Calculate diff
                 exp_list.push(time_diff);
             }
             console.log(exp_list);
 
-            // writeToCsv(TESTNET + "_" + EXPERIMENT_NAME, exp_list); // OOO
+
+            // ##################### FILE WRITER ##################### //
             writeToCsv(TESTNET + "_" + temp_experiment_name[j], exp_list); // XXX     
+            // writeToCsv(TESTNET + "_" + EXPERIMENT_NAME, exp_list); // OOO
         }
 
     }).timeout(999999999999);
